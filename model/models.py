e5f2d1bbd4a6dca818818b5b246354cc244290a5
Ivan Šarić <ivan@path-variable.com>
2023-11-03 20:58:16 +0100

    adds new models to models.py

--- model/models.py
+++ model/models.py
@@ -15,6 +15,7 @@ class FileUpload:
                            "externalId": self.external_id, "externalIdType": self.external_id_type})
 
 
+# Response DTO for searching external Ids
 class IdPair:
     topic = "patient-id-results"
 
@@ -28,6 +29,7 @@ class IdPair:
         return [IdPair(item['external_id'], item['external_id_type']) for item in lst]
 
 
+# Request DTO for searching external Ids
 class IdSearch:
     topic = "patient-id-search"
 
@@ -39,6 +41,7 @@ class IdSearch:
         return json.dumps({"page": self.page, "pageSize": self.page_size})
 
 
+# Request DTO for searching patient records
 class RecordSearch:
     topic = "patient-record-search"
 
@@ -48,3 +51,27 @@ class RecordSearch:
 
     def to_payload(self):
         return json.dumps({"externalId": self.external_id, "externalIdType": self.external_id_type})
+
+
+# Part of the Patient Record DTO
+class MedicalRecord:
+    def __init__(self, file_contents, file_format, external_id, external_id_type):
+        self.file_contents = json.loads(file_contents)
+        self.file_format = file_format
+        self.external_id = external_id
+        self.external_id_type = external_id_type
+
+
+# Response DTO for searching patient records
+class PatientRecord:
+    topic = "patient-record-results"
+
+    def __init__(self, json_string):
+        json_dict = json.loads(json_string)
+        self.id = json_dict['id']
+        self.records = [MedicalRecord(item['file_contents'], item['file_format'], item['external_id'],
+                                      item['external_id_type']) for item in json_dict['records']]
+
+    @staticmethod
+    def load_record(json_string):
+        return PatientRecord(json.loads(json_string))
